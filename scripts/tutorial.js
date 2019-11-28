d7446d50f546dac5ef29344b4624afd441e03d9d
Rami Alkawadri <ramialkawadri@yahoo.com>
2019-11-28 17:08:04 +0100

    Better using of IIFE and better responsive style

--- scripts/tutorial.js
+++ scripts/tutorial.js
@@ -1,15 +1,15 @@
-// The tutorial overlay
-const tutorial = document.getElementById('tutorial');
-
-// The finish button on the totrial
-const finishButton = document.getElementById('finish-tutorial');
-
-finishButton.addEventListener('click', () => {
-  localStorage.setItem('tutorial-seen', true);
-  tutorial.style.display = 'none';
-});
-
 // An IIFE to see if the tutorial has already been seen
 (() => {
+  // The tutorial overlay
+  const tutorial = document.getElementById('tutorial');
+
+  // The finish button on the totrial
+  const finishButton = document.getElementById('finish-tutorial');
+
+  finishButton.addEventListener('click', () => {
+    localStorage.setItem('tutorial-seen', true);
+    tutorial.style.display = 'none';
+  });
+
   if (localStorage.getItem('tutorial-seen')) tutorial.style.display = 'none';
 })();
